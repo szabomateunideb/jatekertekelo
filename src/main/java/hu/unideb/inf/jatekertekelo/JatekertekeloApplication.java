b00fef0bf96e369421e4f762912da664ce2aa304
Máté <szabo.mate@inf.unideb.hu>
2025-09-24 11:31:40 +0200

    init

---
+++ src/main/java/hu/unideb/inf/jatekertekelo/JatekertekeloApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.jatekertekelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatekertekeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatekertekeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/jatekertekelo/JatekertekeloApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.jatekertekelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatekertekeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
